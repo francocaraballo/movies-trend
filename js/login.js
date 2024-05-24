29709a9ab0ab8c8cb385d23f9ef7756c77d3e221
Franco Caraballo <franconcaraballo@gmail.com>
2024-05-24 16:21:48 -0300

    css y html finalizados

---
+++ js/login.js
@@ -0,0 +1,10 @@
+// const form = document.querySelector('form');
+// form.addEventListener('submit', (e) => {
+//     e.preventDefault();
+
+//     // Validar email
+//     const email = document.getElementById('email');
+//     const emailError = document.getElementById('email-error');
+//     // Validar contrasena
+
+// })
